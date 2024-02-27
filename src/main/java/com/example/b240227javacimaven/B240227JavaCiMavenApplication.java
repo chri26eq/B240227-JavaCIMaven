7f88cf2eefbbd3d23ad8d3bb90519d1fb9ac90d7
chri26eq <chri26eq@stud.kea.dk>
2024-02-27 14:20:45 +0100

    Initial commit

---
+++ src/main/java/com/example/b240227javacimaven/B240227JavaCiMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.b240227javacimaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B240227JavaCiMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B240227JavaCiMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/b240227javacimaven/B240227JavaCiMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b240227javacimaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B240227JavaCiMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
